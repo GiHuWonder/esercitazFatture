758e62892ad6be2f100d392ed05fabc4dc7dd02f
Michele <michele.lopardo@wonder-sys.com>
2020-06-09 19:44:29 +0200

    versione iniziale

---
+++ src/Fattura.java
@@ -0,0 +1,27 @@
+public class Fattura {
+    private String id;
+    private String descrizione;
+    private double importo;
+
+    void setId(String id) {
+        this.id = id;
+    }
+    String getId() {
+        return id;
+    }
+    void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+    String getDescrizione() {
+        return descrizione;
+    }
+    void setImporto(double importo) {
+        this.importo = importo;
+    }
+    double getImporto() {
+        return importo;
+    }
+    double getImportoIvato() {
+        return importo * 1.11;
+    }
+}
---
+++ src/ProvaFatture.java
@@ -0,0 +1,17 @@
+public class ProvaFatture {
+    public static void main(String[] arg) {
+        //System.out.println("inizio");
+        Fattura fattura1 = new Fattura();
+        fattura1.setId("A2-2019");
+        fattura1.setDescrizione("Prestazione occasionale");
+        System.out.println(fattura1.getId()+"  "+fattura1.getDescrizione());
+        double importo = 1500;
+        fattura1.setImporto(importo);
+        double importoIvato = fattura1.getImportoIvato();
+        if (importoIvato == 0.0) {
+            System.out.println("l'importo non è stato assegnato!");
+        } else {
+            System.out.println("l'importo di " + importo + " con IVA al 22% è " + importoIvato);
+        }
+    }
+}
